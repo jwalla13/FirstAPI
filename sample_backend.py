28df82e49c4e8e00ab9c67506b7a37bb45588f3b
Jonathan Wallach <jwalla13@calpoly.edu>
2021-01-14 13:45:52 -0800

    Link to Front End and Add Various Features

--- sample_backend.py
+++ sample_backend.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import random
+import string
 
 app = Flask(__name__)
 CORS(app)
@@ -23,20 +25,23 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      userToAdd['id']= generate_id()
       users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp = jsonify(success=True, user=userToAdd)
+      resp.status_code = 201 #optionally, you can always set a response code.
       # 200 is the default code for a normal response
       return resp
-   if request.method == 'DELETE':
-     search_username = request.args.get('name')
-     if search_username:
-       for user in users['users_list']:
-         if user['name'] == search_username:
-           users['users_list'].remove(user)
-           resp = jsonify(success=True)
-           return resp
+   elif request.method == 'DELETE':
+      userToDelete = request.get_json()
+      users['users_list'].remove(userToDelete)
+      resp = jsonify(success=True)
+      resp.status_code = 200
+      return resp
 
+def generate_id():
+   letters = ''.join(random.choice(string.ascii_lowercase) for i in range(3))
+   num = str(random.randint(100,999))
+   return letters + num
 
 @app.route('/users/<id>')
 def get_user(id):
